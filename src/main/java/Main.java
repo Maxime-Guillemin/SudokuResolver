6fce8e3b038829b9cd8767b7e87c06344b56f85d
VoltalitSama <gregory.monsterlet182@gmail.com>
2023-06-27 11:04:34 +0200

    Checkstyle main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class Main {
-    public static void main(String[] args) {
+/**
+ * The Main class represents the entry point for solving a Sudoku puzzle.
+ * It reads a Sudoku grid from a file, solves the puzzle, and prints the result.
+ */
+public final class Main {
+    /**
+     * Constructeur privé pour empêcher l'instanciation de la
+     * classe utilitaire.
+     * Lève une UnsupportedOperationException pour indiquer
+     * que cette classe ne peut pas être instanciée.
+     *
+     * @throws UnsupportedOperationException si une tentative
+     * est faite pour instancier la classe utilitaire
+     */
+    private Main() {
+        throw new UnsupportedOperationException(
+                "This is a utility class and cannot be instantiated.");
+    }
+    /**
+     * The main method is the entry point for the program.
+     * It reads a Sudoku grid from a file, solves the puzzle,
+     * and prints the result.
+     *
+     * @param args command line arguments (not used in this program)
+     */
+    public static void main(final String[] args) {
         String fichierGrille = "grille/sudoku16-a.txt";
 
         try (InputStream in = getInputStreamFromRelativePath(fichierGrille)) {
@@ -27,11 +51,17 @@ public class Main {
                 System.out.println("La grille ne peut pas être résolue.");
             }
         } catch (IOException e) {
-            System.out.println("Une erreur s'est produite lors de la lecture du fichier de grille :");
+            System.out.println("Une erreur s'est produite lors "
+                    +
+                    "de la lecture du fichier de grille :");
             e.printStackTrace();
-        } catch (ElementInterditException | ValeurInitialeModificationException |
-                 HorsBornesException | ValeurImpossibleException e) {
-            System.out.println("Une exception s'est produite lors de la résolution du sudoku :");
+        } catch (ElementInterditException
+                 | ValeurInitialeModificationException
+                 | HorsBornesException
+                 | ValeurImpossibleException e) {
+            System.out.println("Une exception s'est produite "
+                    +
+                    "lors de la résolution du sudoku :");
             e.printStackTrace();
         }
     }
@@ -43,11 +73,14 @@ public class Main {
      * @return InputStream correspondant au fichier
      * @throws IOException si une erreur de lecture du fichier se produit
      */
-    private static InputStream getInputStreamFromRelativePath(String relativePath) throws IOException {
+    private static InputStream
+    getInputStreamFromRelativePath(final String relativePath)
+            throws IOException {
         // Obtient le répertoire de travail actuel de l'application
         String workingDirectory = System.getProperty("user.dir");
 
-        // Construit le chemin absolu en concaténant le répertoire de travail avec le chemin relatif
+        // Construit le chemin absolu en concaténant le répertoire
+        // de travail avec le chemin relatif
         Path path = Paths.get(workingDirectory, relativePath);
 
         // Ouvre un InputStream à partir du chemin absolu
